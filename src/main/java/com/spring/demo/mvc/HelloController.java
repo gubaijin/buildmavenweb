70995d7d077b2478aa3ceab7fc9df97054ba5208
Kevin <gubaijing2004@163.com>
2016-06-05 20:15:52 +0800

    finish springmvc

--- src/main/java/com/spring/demo/mvc/HelloController.java
+++ src/main/java/com/spring/demo/mvc/HelloController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/mvc")
-public class GreetingController {
+public class HelloController {
 
-	@RequestMapping("/greeting")
+	@RequestMapping("/hello")
 	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
 			Model model) {
 		model.addAttribute("name", name);
-		return "/demo/greeting";
+		return "/demo/hello";
 	}
 
 }
--- src/main/webapp/WEB-INF/demo/hello.jsp
+++ src/main/webapp/WEB-INF/demo/hello.jsp
@@ -1,12 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <title>Insert title here</title>
 </head>
 <body>
-${name }2
+Hello，${name}！
 </body>
 </html>
