27048449d1b1b8611c17c5925c84ad8f5f3f6c35
gubaijing2004@163.com <gubaijing2004@163.com>
2016-07-14 18:03:25 +0800

    task

--- src/main/java/com/spring/demo/hello/Application.java
+++ src/main/java/com/spring/demo/hello/Application.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
 
 @Configuration//等价 与XML中配置beans
 @ComponentScan//配置spring扫描
---
+++ src/main/java/com/spring/demo/task/Task.java
@@ -0,0 +1,12 @@
+package com.spring.demo.task;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Task {
+
+	public void doCheck() {
+		System.out.println(1);
+	}
+
+}
